457d913f6f5392b9db6009a9c08e2f88c11e2353
Menashe Gezelkopy <menashe.gezelkopy@gmail.com>
2017-03-23 15:38:51 +0100

    version41

---
+++ app/controllers/home.js
@@ -0,0 +1,38 @@
+var express = require('express'),
+  router = express.Router(),
+  Article = require('../models/article');
+
+module.exports = function (app) {
+  app.use('/', router);
+};
+
+router.get('/', function (req, res, next) {
+  var articles = [new Article(), new Article()];
+    res.render('index', {
+      title: 'Generator-Express MVC',
+      articles: articles
+    });
+});
+
+
+router.get('/ValidateEmail/:email', function(req, res) {
+
+    var email = req.params.email; 
+
+    res.send(email);
+
+    verifyEmail(email);
+
+            function verifyEmail(email) {
+            var status = "false";     
+            var emailRegEx = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+                if (email.search(emailRegEx) == -1) {
+                   // alert("Please enter a valid email address.");
+                } else {
+                 //   alert("Woohoo!  The email address is in the correct format and they are the same.");
+                    status = "true";
+                }
+                return status;
+            }
+
+});
